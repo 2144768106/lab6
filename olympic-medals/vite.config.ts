8faf7970d79c50384e5075b8a08e78ec574d7848
2144768106 <ylj2144768106@outlook.com>
2024-11-19 18:30:17 +0800

    添加了vercel部署

--- olympic-medals/vite.config.ts
+++ olympic-medals/vite.config.ts
@@ -3,15 +3,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  server: {
-    cors: true
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser'
   }
 })
